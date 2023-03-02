d00b85f9c155e44dd2a5b4cbc2b1dcc634f5cc8a
Kenneth Ekow Inkum <xanthosoma1989@gmail.com>
2023-03-02 16:59:34 +0000

    IWD

--- resources/views/layouts/partials/testimonials.blade.php
+++ resources/views/layouts/partials/testimonials.blade.php
@@ -1,4 +1,4 @@
-<div class="bg-primary testimonial py-5 my-5 wow fadeInUp brand-logo" data-wow-delay="0.1s">
+<div class="bg-primary testimonial py-5 my-5 wow fadeInUp brand-logocolor" data-wow-delay="0.1s">
     <div class="container py-5 px-lg-5">
         <div class="owl-carousel testimonial-carousel">
             <div class="testimonial-item bg-transparent rounded text-white p-4"
